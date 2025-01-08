9636d7cdd3986009462b8f0d7e5c52c456172683
aquaimpact <oweien12@gmail.com>
2025-01-08 22:36:10 +0800

    init commit

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 import os
+
 import logging
 from dotenv import load_dotenv
-from telegram import Update
+
+from telegram import Update, Bot
 from telegram.constants import ParseMode
-from telegram.ext import Application, CommandHandler, ContextTypes, MessageHandler, filters
+from telegram.ext import Application, CommandHandler
+
 from webscraper import webscraper
 
 # Automation Libraries
@@ -17,7 +20,8 @@ TOKEN = os.getenv('BOT_API_TOKEN')
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 logger = logging.getLogger(__name__)
 
-async def sendTemplate(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+
+async def sendTemplate(update, context):
     """Send a message and photo when the command /start is issued."""
     chat_id = -4634623504  # Get chat ID of the user
                            # -2287006194
@@ -40,21 +44,18 @@ async def sendTemplate(update: Update, context: ContextTypes.DEFAULT_TYPE) -> No
         logger.error(f"Error sending photo: {e}")
         await context.bot.send_message(chat_id=chat_id, text="Failed to send the photo. Please try again later.")   
 
+def register_handlers(app: Application) -> None:
+    sendTemplate_handler = CommandHandler("sendTemplate", sendTemplate)
+    app.add_handler(sendTemplate_handler)
 
-if __name__ == "__main__":
-    """Start the bot."""
-    # Create the Application and pass it your bot's token.
+
+def main() -> None:
     application = Application.builder().token(TOKEN).build()
+    register_handlers(application)
 
-    # on different commands - answer in Telegram
-    application.add_handler(CommandHandler("sendTemplate", sendTemplate))
-
-    # Set up the scheduler
+    logger.info("Starting polling...")
+    application.run_polling()
     
 
-
-    # on non command i.e message - echo the message on Telegram
-    # application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, echo))
-
-    # Run the bot until the user presses Ctrl-C
-    application.run_polling(allowed_updates=Update.ALL_TYPES)
+if __name__ == "__main__":
+    main()
